bcede7751c8e653c268f9adae3e923740fc3f99b
Tomas Perez <tomaspz@gmail.com>
2020-05-05 22:56:57 -0400

    change settings in server.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Shared/Navbar';
 import Footer from './components/Shared/Footer';
